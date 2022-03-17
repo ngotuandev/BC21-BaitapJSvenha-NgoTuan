a460c797b674adb551147d81f7d52ee3516ed16a
riokez <riokez3377@gmail.com>
2022-03-17 10:16:47 +0700

    Sửa bài thuế 17/3

--- thuethunhapcanhan.js
+++ thuethunhapcanhan.js
@@ -9,18 +9,18 @@ document.getElementById("tinhThueThuNhap").addEventListener("click",function(){
     if (thuNhapChiuThue<=(60*trieu)){
         thueCaNhan=thuNhapChiuThue*0.05;
     } else if (thuNhapChiuThue<=(120*trieu)){
-        thueCaNhan=thuNhapChiuThue*0.1  ;
+        thueCaNhan=(60*trieu*0.05)+(thuNhapChiuThue-60*trieu)*0.1  ;
     } else if (thuNhapChiuThue<=(210*trieu)){
-        thueCaNhan=thuNhapChiuThue*0.15;
+        thueCaNhan=(60*trieu*0.05)+(60*trieu*0.1)+(thuNhapChiuThue-120*trieu)*0.15;
     } else if (thuNhapChiuThue<=(384*trieu)){
-        thueCaNhan=thuNhapChiuThue*0.2;
+        thueCaNhan=(60*trieu*0.05)+(60*trieu*0.1)+(90*trieu*0.15)+(thuNhapChiuThue-210*trieu)*0.2;
     } else if (thuNhapChiuThue<=(624*trieu)){
-        thueCaNhan=thuNhapChiuThue*0.25;
+        thueCaNhan=(60*trieu*0.05)+(60*trieu*0.1)+(90*trieu*0.15)+(174*trieu*0.2)+(thuNhapChiuThue-384*trieu)*0.25;
 
     } else if (thuNhapChiuThue<=(960*trieu)){
-        thueCaNhan=thuNhapChiuThue*0.3;
+        thueCaNhan=(60*trieu*0.05)+(60*trieu*0.1)+(90*trieu*0.15)+(174*trieu*0.2)+(240*trieu*0.25)+(thuNhapChiuThue-624*trieu)*0.3;
     } else {
-        thueCaNhan=thuNhapChiuThue*0.35;
+        thueCaNhan=(60*trieu*0.05)+(60*trieu*0.1)+(90*trieu*0.15)+(174*trieu*0.2)+(240*trieu*0.25)+(336*trieu*0.3)+(thuNhapChiuThue-960*trieu)*0.35;
     }
     document.getElementById("ketQuaThue").innerHTML=`<span> Thuế thu nhập cá nhân của (anh/chị) ${hoVaTen} là ${thueCaNhan.toLocaleString()} đồng </span> `
 })
